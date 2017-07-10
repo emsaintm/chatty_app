9f9d9455e5f756a36aa7eca569f65545f3c89b2f
EMILY <stmartin.emily@gmail.com>
2017-07-10 09:03:51 -0500

    Add ability to create a scribble

--- app/controllers/scribbles_controller.rb
+++ app/controllers/scribbles_controller.rb
@@ -1,13 +1,41 @@
 class ScribblesController < ApplicationController
   def index
+    @scribbles = Scribble.all
   end
 
   def show
   end
 
   def new
+    @scribble = Scribble.new
+  end
+
+  def create
+    @scribble = Scribble.new(scribble_params)
+  
+    respond_to do |format|
+      if @scribble.save 
+        format.html { redirect_to @scribble, notice: "Here's your scribble..." }
+      else
+        format.html { render :new }
+      end
+    end
+
   end
 
   def edit
   end
+
+
+
+private
+
+def scribble_params
+  params.require(:scribble).permit(:message, :user_id)
 end
+
+
+end
+
+
+
