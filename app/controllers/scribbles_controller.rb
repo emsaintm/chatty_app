53111f8c124c94293e563f919c784a27d0a7a457
EMILY <stmartin.emily@gmail.com>
2017-07-10 10:04:05 -0500

    Add ability to update a scribble

--- app/controllers/scribbles_controller.rb
+++ app/controllers/scribbles_controller.rb
@@ -1,13 +1,13 @@
 class ScribblesController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_scribble, only: [:edit, :show, :update]
 
   def index
     @scribbles = Scribble.all
   end
 
   def show
-    @scribble = Scribble.find(params[:id])
-    # model gets you what you need ^ capitalized one is from model
+    # before action gets @scribble we want to show
   end
 
   def new
@@ -24,15 +24,31 @@ class ScribblesController < ApplicationController
         format.html { render :new }
       end
     end
-
   end
 
   def edit
+    # get the tweet we want to edit from the model and send it to the edit view
+    # set_scribble before action gets the scribble we want to edit
+  end
+
+  def update
+    # before action gets the scribble we want to update
+    # update it with new info (from params)
+    respond_to do |format|
+      if @scribble.update(scribble_params)
+        format.html { redirect_to @scribble, notice: "You changed that scribble."}
+      else
+        format.html { render :edit }
+      end
+    end
   end
 
 
-
 private
+
+def set_scribble
+  @scribble = Scribble.find(params[:id])  
+end
 
 def scribble_params
   params.require(:scribble).permit(:message, :user_id)
